737ae209004ebea03da552b75ae82b34e802beae
ismael-henrique-dev <ismael.henrique.dev@gmail.com>
2025-12-26 17:48:49 -0300

    [M1-01/01-1] feat: add Origin UI components and core components

--- src/app/platform/home/page.tsx
+++ src/app/platform/home/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/core/button'
+
 export default function Home() {
   return (
     <div>
-      <h1 className="font-rubik">Home - Platform</h1>
+      <h1 className='font-rubik'>Home - Platform</h1>
+      <Button isLoading>asasas</Button>
     </div>
   )
 }
